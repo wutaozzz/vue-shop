9ba6b4ef4117bf60400b8d3fbe06723eff99578d
tao_wu <tao_wu@intsig.net>
2021-08-24 23:14:34 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 将axios挂载到vue组件的原型上
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
